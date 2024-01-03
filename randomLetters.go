07386fc2606fced392b3cf1bfba730081d334539
Decorentin <corentindelpree@gmail.com>
2024-01-03 13:43:06 +0100

    test2

--- randomLetters.go
+++ randomLetters.go
@@ -2,32 +2,25 @@ package hangmanpackage
 
 import (
 	"math/rand"
+	"strings"
 )
 
 // function that calculates the number of letters to be revealed and prints the word with the revealed letters and "_".
-func RandomLetters(data *HangManData) {
-	nRandomLetters := (len(data.ToFind) / 2) - 1
-	var tabLetters []string
-	var word string
-	for i := 0; i < len(data.ToFind); i++ {
-		word += "_"
+func RandomLetters(words string) string {
+	numbreveal := (len(words) / 2) - 1
+	tabword := make([]string, len(words))
+	for i := range tabword {
+		tabword[i] = "_"
 	}
-	i := 0
-	for i < nRandomLetters {
-		randomIndex := rand.Intn(len(data.ToFind)) //choose random letters in the word
-		randomLetter := string(data.ToFind[randomIndex])
-		data.TabInput = append(data.TabInput, randomLetter)
-		found := 0
-		for _, letter := range tabLetters {
-			if letter == randomLetter {
-				found = 1 //Set 'found' to 1 if the letter is found in 'tabLetters
+	for i := 0; i < numbreveal; i++ {
+		max := len(words) - 1
+		indexrandomword := rand.Intn(max)
+		randomletter := string(words[indexrandomword])
+		for j := 0; j < len(words); j++ {
+			if string(words[j]) == randomletter {
+				tabword[j] = randomletter
 			}
 		}
-		if found != 1 {
-			tabLetters = append(tabLetters, randomLetter) // replace the corresponding "_" in 'word',
-			word = ReplaceLetter(data.ToFind, word, randomLetter)
-			i++
-		}
 	}
-	data.Word = word
+	return strings.Join(tabword, "")
 }
