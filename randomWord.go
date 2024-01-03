bf50338386c8e5e8fde088ae5cf166b93f7f085f
Decorentin <corentindelpree@gmail.com>
2024-01-03 12:47:42 +0100

    fix

--- randomWord.go
+++ randomWord.go
@@ -8,7 +8,7 @@ import (
 )
 
 // function to generate a random word in the file specified by the user
-func RandomWord(data *HangManData) {
+func RandomWord() string {
 	file := os.Args[1]
 	content, err := os.ReadFile(file) //opens user-selected txt
 	if err != nil {
@@ -16,5 +16,5 @@ func RandomWord(data *HangManData) {
 	}
 	words := strings.Split(string(content), "\n") //choose a random word in the txt
 	randomWord := words[rand.Intn(len(words))]
-	data.ToFind = randomWord
+	return randomWord
 }
