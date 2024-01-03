3b7dc27c8f5854a72491bd9564969767c1707288
Decorentin <corentindelpree@gmail.com>
2024-01-03 13:01:15 +0100

    fix

--- randomWord.go
+++ randomWord.go
@@ -1,20 +1,33 @@
 package hangmanpackage
 
 import (
+	"bufio"
 	"log"
 	"math/rand"
 	"os"
-	"strings"
+	"time"
 )
 
 // function to generate a random word in the file specified by the user
 func RandomWord() string {
-	file := os.Args[1]
-	content, err := os.ReadFile(file) //opens user-selected txt
+	dictionaryPath := os.Args[1]
+	var tabword []string
+	readFile, err := os.Open(dictionaryPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	words := strings.Split(string(content), "\n") //choose a random word in the txt
-	randomWord := words[rand.Intn(len(words))]
-	return randomWord
+	fileScanner := bufio.NewScanner(readFile) // Crée un scanner pour lire le fichier.
+	fileScanner.Split(bufio.ScanLines)        // Divise le fichier en lignes.
+	// Parcours chaque ligne du fichier.
+	for fileScanner.Scan() {
+		// Ajoute le texte de la ligne actuelle à la slice trouverlemot.
+		tabword = append(tabword, fileScanner.Text())
+	}
+	readFile.Close()
+	rand.Seed(time.Now().UnixNano()) // Initialize the random number generator
+	max := len(tabword)
+	indexrandomword := rand.Intn(max)
+	randomword := tabword[indexrandomword]
+
+	return randomword
 }
