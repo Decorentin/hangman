103f9de95010b4386fdf4351ab0cb6f422b52dca
Decorentin <corentindelpree@gmail.com>
2024-01-04 19:03:56 +0100

    fix

--- randomLetters.go
+++ randomLetters.go
@@ -12,15 +12,28 @@ func RandomLetters(words string) string {
 	for i := range tabword {
 		tabword[i] = "_"
 	}
+
+	// Utilise un ensemble pour suivre les lettres déjà révélées
+	revealedLetters := make(map[string]bool)
+
 	for i := 0; i < numbreveal; i++ {
-		max := len(words) - 1
-		indexrandomword := rand.Intn(max)
-		randomletter := string(words[indexrandomword])
+		// Choisis une lettre aléatoire qui n'a pas encore été révélée
+		randomIndex := rand.Intn(len(words))
+		for revealedLetters[string(words[randomIndex])] {
+			randomIndex = rand.Intn(len(words))
+		}
+
+		randomLetter := string(words[randomIndex])
+		revealedLetters[randomLetter] = true
+
+		// Remplace la première occurrence non révélée dans tabword
 		for j := 0; j < len(words); j++ {
-			if string(words[j]) == randomletter {
-				tabword[j] = randomletter
+			if string(words[j]) == randomLetter && tabword[j] == "_" {
+				tabword[j] = randomLetter
+				break
 			}
 		}
 	}
+
 	return strings.Join(tabword, "")
 }
