38043da7a719d42bffdbceee93089cd42b116cbe
Decorentin <corentindelpree@gmail.com>
2024-01-07 23:23:41 +0100

    add bool

--- replaceLetter.go
+++ replaceLetter.go
@@ -1,17 +1,22 @@
 package hangmanpackage
 
 // Replace the letter of the partially filled word with the chosen letter if it is in the word
-func ReplaceLetter(word, currentWord, letter string) string {
+func ReplaceLetter(word, currentWord, letter string, attempts int) (string, bool) {
 	result := ""
+	inTheWord := false
+
 	for i := 0; i < len(word); i++ {
 		if string(word[i]) == letter {
 			result += letter
+			inTheWord = true
 		} else {
 			result += string(currentWord[i])
 		}
 	}
+
 	if len(currentWord) > len(word) {
 		result += currentWord[len(word):]
 	}
-	return result
+
+	return result, inTheWord
 }
