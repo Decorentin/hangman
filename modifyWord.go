7512fe1ee2e85c887c4133b9f5f4350675c864c4
Decorentin <corentindelpree@gmail.com>
2024-01-07 17:23:21 +0100

    fix

--- modifyWord.go
+++ modifyWord.go
@@ -5,18 +5,19 @@ import (
 	"strings"
 )
 
-func ModifyWord(wordtofind, actualword, letter string) string {
-	// Vérifie si la lettre est présente dans le mot
+func ModifyWord(wordtofind, actualword, letter string, attempts *int) string {
 	if strings.Contains(wordtofind, letter) {
-		// Met à jour actualword en ajoutant la lettre à la position correspondante
 		for i, r := range wordtofind {
 			if r == rune(letter[0]) {
 				actualword = actualword[:i] + letter + actualword[i+1:]
 			}
 		}
+		fmt.Println(*attempts)
 		fmt.Println(actualword)
 		return actualword
 	}
+	*attempts -= 1
+	fmt.Println(*attempts)
 	fmt.Println(actualword)
 	return actualword
 }
