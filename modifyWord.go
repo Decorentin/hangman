70f98d09247fbe22f63b78b541a481607957a9e0
Decorentin <corentindelpree@gmail.com>
2024-01-07 17:26:19 +0100

    fix attempts bug

--- modifyWord.go
+++ modifyWord.go
@@ -5,19 +5,21 @@ import (
 	"strings"
 )
 
-func ModifyWord(wordtofind, actualword, letter string, attempts *int) string {
+func ModifyWord(wordtofind, actualword, letter string, attempts int) (string, int) {
+	// Vérifie si la lettre est présente dans le mot
 	if strings.Contains(wordtofind, letter) {
+		// Met à jour actualword en ajoutant la lettre à la position correspondante
 		for i, r := range wordtofind {
 			if r == rune(letter[0]) {
 				actualword = actualword[:i] + letter + actualword[i+1:]
 			}
 		}
-		fmt.Println(*attempts)
+		fmt.Println(attempts)
 		fmt.Println(actualword)
-		return actualword
+		return actualword, attempts
 	}
-	*attempts -= 1
-	fmt.Println(*attempts)
+	attempts -= 1
+	fmt.Println(attempts)
 	fmt.Println(actualword)
-	return actualword
+	return actualword, attempts
 }
