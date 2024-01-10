7353099b27406b68e0a38b5d56bdc19d73d0b4ec
Decorentin <corentindelpree@gmail.com>
2024-01-10 17:57:39 +0100

    add a fonction for web level

---
+++ fileRandomWord.go
@@ -0,0 +1,19 @@
+package hangmanpackage
+
+import (
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+)
+
+// function to generate a random word in the file specified by the user
+func fileRandomWord(file string) string {
+	content, err := os.ReadFile(file) //opens user-selected txt
+	if err != nil {
+		log.Fatal(err)
+	}
+	words := strings.Split(string(content), "\n") //choose a random word in the txt
+	randomWord := words[rand.Intn(len(words))]
+	return randomWord
+}
